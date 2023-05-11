00b550bc8d906e68e99956a59d16287c02e6861c
kitsune_yk <agvksl543@gmail.com>
2023-05-11 10:53:20 +0900

    refactor: 不要なimportを削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import ReactCardFlip from "react-card-flip";
 import { Footer } from "./components/Footer";
 import { Interest } from "./components/Interest";
